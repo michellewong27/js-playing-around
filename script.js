c25d8896f9c64963f02736d7fd1ce501e9d9445f
Michelle Wong <michellekwong2@gmail.com>
2020-03-25 15:14:11 -0400

    Fixed html lay out of page, started JS color event listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//selected color by its class name
+const colorBtn = document.querySelector('.colorBtn');
+//selecting by tag name of body
+const bodyBcg = document.querySelector('body');
 
+//array of background colors well use
+const colors = ['yellow', 'red', 'green', '#3b5998'];
+
+//add event listener on click go to callbackfunction
+colorBtn.addEventListener('click', changeColor);
+
+function changeColor (){
+    //grabbing body, selecting style, and background color
+    bodyBcg.style.backgroundColor = colors[2];
+}
